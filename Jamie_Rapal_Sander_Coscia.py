5857a05132a45f9cc613f2ad9dabba6ceb493a2a
Jamie-R24 <jamierapal@gmail.com>
2024-10-01 14:59:35 -0400

    Finished 3rd EDA. Graphed Travel spending month by month for 2019 only. Looked at transaction amount totals and transaction numbers

--- Jamie_Rapal_Sander_Coscia.py
+++ Jamie_Rapal_Sander_Coscia.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('credit_card_transactions.csv')
+df = df.head(1296675)
 
 #------------------------------
 #-------- Task 2 --------------
@@ -33,9 +34,9 @@ for index, value in enumerate(merchant_summary['Transaction_Count']):
 
 plt.tight_layout()
 plt.show()
-#----------------------------------------
 
 #---- Group by Transaction Sum Total by Category------
+
 merchant_summary = merchant_summary.sort_values(by='Total_Amount', ascending=False)
 plt.figure(figsize=(12,6))
 sns.barplot(x = 'Total_Amount', y = 'category', data = merchant_summary, palette = 'viridis')
@@ -48,6 +49,34 @@ for index, value in enumerate(merchant_summary['Total_Amount']):
 
 plt.tight_layout()
 plt.show()
-#-------------------------------------------------------
 
+#---------Analyzing Travel Spending (2019 only)-----------------------
+travel_df = df[df['category'] == 'travel'].copy()
 
+travel_df['trans_date_trans_time'] = pd.to_datetime(travel_df['trans_date_trans_time'])
+
+travel_df = travel_df[travel_df['trans_date_trans_time'] < '2020-01-01'] #No full 2020 year end data so cut it out
+
+travel_df['Month'] = travel_df['trans_date_trans_time'].dt.to_period('M').astype(str)
+
+travel_summary = travel_df.groupby('Month').agg(
+    Total_Amount = ('amt', 'sum'),
+    Transaction_Count = ('amt', 'count')
+).reset_index()
+
+plt.figure(figsize=(14, 7))
+
+plt.subplot(2, 1, 1)
+sns.lineplot(data=travel_summary, x='Month', y='Total_Amount', marker='o')
+plt.title('Total Transaction Amount for Travel Category (Month by Month)')
+plt.xlabel('Month')
+plt.ylabel('Total Amount')
+
+plt.subplot(2, 1, 2)
+sns.lineplot(data=travel_summary, x='Month', y='Transaction_Count', marker='o', color='orange')
+plt.title('Transaction Count for Travel Category (Month by Month)')
+plt.xlabel('Month')
+plt.ylabel('Transaction Count')
+
+plt.tight_layout()
+plt.show()
